996b67d99f9662442b220aec2e4376aa0cd5d93a
Richard Deleon-Romero <49032140+RjDeleon1@users.noreply.github.com>
2020-04-14 17:18:44 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,320 @@
+import React, { useState, useEffect } from 'react';
+import 'react-native-gesture-handler';
+import { StyleSheet, Text, View,  Button, Alert, Image, TextInput, TouchableHighlight, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { Component } from 'react';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Firebase from 'firebase';
+
+
+let config = {
+  apiKey: "AIzaSyDAyyaH__1lkyyl_8g2FdjMmHSjNnm2noY",
+  authDomain: "final-projecft-2c2e2.firebaseapp.com",
+  databaseURL: "https://final-projecft-2c2e2.firebaseio.com",
+  projectId: "final-projecft-2c2e2",
+  storageBucket: "final-projecft-2c2e2.appspot.com",
+  messagingSenderId: "807770925390",
+  appId: "1:807770925390:web:33a26127bd37566c63961f",
+    measurementId: "G-NWDF61RSBD"
+};
+
+let app = Firebase.initializeApp(config);
+export const db = app.database();
+
+let listPurchases = db.ref('/purchases/');
+
+
+function HomeScreen({ navigation }){
+  return(
+    <View style={styles.container}>
+      <Image style={styles.image} source={require('./assets/DTCC.png')} />
+      <Text style={styles.text}>  Welcome to this Expense Tracker App</Text>
+      <Text style={styles.text}> Add expenses to the tracker and we will display all expenses that you have entered</Text>
+      <Text style={styles.text}> Click on any of the following links to continue</Text>
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => navigation.navigate('Summary of expenses')}
+        >
+          <Text style={styles.buttonText}>Summary</Text>
+      
+      
+      </TouchableHighlight>
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => navigation.navigate('New Expenses')}
+        >
+          <Text style={styles.buttonText}>Add New Expenses</Text>
+
+      </TouchableHighlight>
+
+
+    </View>
+  );
+}
+
+function Summary({ navigation }){
+
+
+  return(
+  <View style={styles.container}>
+      <Text style={styles.text}>Welcome To The Expenses Summary Page</Text>
+      <Text style={styles.text}>Here you will find all expenses that have been added!</Text>
+
+
+
+
+
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+      
+      
+      </TouchableHighlight>
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => navigation.navigate('New Expenses')}
+        >
+          <Text style={styles.buttonText}>Add New Expenses</Text>
+
+      </TouchableHighlight>
+
+  </View>
+  );
+}
+
+
+class NewExpenses extends Component{
+
+  //Constructor
+  constructor(){
+    super();
+    this.state = {
+      value: "", 
+      place: "",
+      type: "",
+      date: "",
+      description:"",
+    };
+
+
+  }
+
+  handleValue= e => {
+    this.setState({
+      value: e.nativeEvent.text
+    });
+  };
+  handleType= e => {
+    this.setState({
+      type: e.nativeEvent.text
+      
+    });
+  };
+  handlePlace= e => {
+    this.setState({
+      place: e.nativeEvent.text
+      
+    });
+  };
+  handleDate= e => {
+    this.setState({
+      date: e.nativeEvent.text
+      
+    });
+  };
+  handleDescription= e => {
+    this.setState({
+      description: e.nativeEvent.text
+      
+    });
+  };
+
+  handleSubmit = () => {
+    let addItem = (Value, Place, Type, Date, Description) => {
+      db.ref('/purchases/').push({
+        value:Value,
+        place:Place,
+        type:Type,
+        date:Date,
+        description:Description,
+      });
+    };
+
+    addItem(this.state.value, this.state.place, this.state.type, this.state.date, this.state.description);
+    Alert.alert('Purhcase saved successfully');
+  };
+
+
+render(){
+  return (
+    <KeyboardAvoidingView behavior="padding" enabled keyboardVerticalOffset='10'>
+    <ScrollView>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to New Expenses screen!</Text>
+      <Text style={styles.text}>Here you will be able to add new expenses to the tracker!</Text>
+      <Text style={styles.text}>Once you are done you can go to the Summary screen to see all other expenses</Text>
+
+              {/* Code for Adding new expenses  */}
+    
+      <Text style={styles.text}>Value of purchase:</Text>
+      <TextInput style={styles.input} 
+      onChange={this.handleValue}
+      keyboardType="numeric"
+      placeholder="Value"
+       />      
+
+      <Text style={styles.text}>Place of purchase:</Text>
+      <TextInput style={styles.input} 
+      onChange={this.handlePlace} 
+      placeholder="Business Name"
+      />      
+
+      <Text style={styles.text}>Type of Purhcase:</Text>
+      <TextInput style={styles.input}
+       onChange={this.handleType}
+       placeholder="Type of Purchase"
+       />      
+
+      <Text style={styles.text}>Date of purchase:</Text>
+      <TextInput style={styles.input} 
+      onChange={this.handleDate} 
+      keyboardType="numbers-and-punctuation"
+      placeholder="Date"
+      />  
+
+      <Text style={styles.text}>Description for the purchase:</Text>
+      <TextInput style={styles.input}
+       onChange={this.handleDescription}
+       placeholder="Description"
+       />         
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={this.handleSubmit}
+        >
+          <Text style={styles.buttonText}>Add</Text>
+
+      </TouchableHighlight>
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => this.props.navigation.navigate('Home')}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+
+      </TouchableHighlight>
+
+      <Text>{''}</Text>
+      <TouchableHighlight
+        style={styles.button}
+          underlayColor="white"
+          onPress={() => this.props.navigation.navigate('Summary of expenses')}
+        >
+          <Text style={styles.buttonText}>Summary</Text>
+
+      </TouchableHighlight>
+
+    </View>
+    </ScrollView>
+    </KeyboardAvoidingView>
+   );
+  }
+}
+
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Summary of expenses" component={Summary} />
+        <Stack.Screen name="New Expenses" component={NewExpenses} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "lightblue",
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  text:{
+    fontSize: 16,
+    marginTop: 25,
+    marginBottom: 25,
+    textAlign: "center",
+  },
+
+  listText:{
+    fontSize:16,
+    marginTop: 10,
+  },
+
+  list:{
+    width:"75%"
+    },
+
+  image:{
+    marginTop: 20,
+    width: "100%",
+    height: 75,
+
+  },
+
+  input: {
+    height: 50,
+    width:250,
+    padding: 4,
+    marginRight: 5,
+    fontSize: 23,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 8,
+    color: 'white'
+  },
+
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
+  },
+
+  button: {
+    height: 45,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+
+  }
+});
